82fb2f5b99aa859c557b5b5cda3a6ea1a53d16a7
Brionesgmm <99777937+Brionesgmm@users.noreply.github.com>
2023-07-24 22:20:15 -0600

    had to change how I was calling the api for the images because pexels was causing an issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import Photo from "./components/Photo";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       {/* <h1 className="siteTitle">Vivi's Animal Cuties</h1> */}
--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,5 +1,5 @@
 import { React, useState, useEffect } from "react";
-import { createClient } from "pexels";
+// import { createClient } from "pexels";
 import Lottie from "lottie-react";
 import loadingCatWaliking from "../assets/loadingCatWalking.json";
 
@@ -8,26 +8,58 @@ const Photo = () => {
   const [pageNumber, setPageNumber] = useState(0);
   const [isLoadingImage, setIsLoadingImage] = useState(true);
 
-  const client = createClient(
-    "FCHHpjKZb6AxS6BCzYSglJdx2ItT9KuYUtBPruiJkE9EypHJErJcbzPv"
-  );
+  //   const client = createClient(
+  //     "FCHHpjKZb6AxS6BCzYSglJdx2ItT9KuYUtBPruiJkE9EypHJErJcbzPv"
+  //   );
 
   const query = "Cute Animals";
 
-  async function fetchPhoto() {
-    setIsLoadingImage(true);
-    const photos = await client.photos.search({
-      query,
-      per_page: 80,
-      page: pageNumber ? 1 : pageNumber,
-    });
-    const totalPages = Math.ceil(photos.total_results / photos.per_page);
-    setPageNumber(Math.floor(Math.random() * totalPages) + 1);
-    const photoArrlength = photos.photos.length;
-    const randomImageNumber = Math.floor(Math.random() * photoArrlength);
-    setAnimalURLImage(photos.photos[randomImageNumber].src.original);
-  }
+  //   async function fetchPhoto() {
+  //     setIsLoadingImage(true);
+  //     const photos = await client.photos.search({
+  //       query,
+  //       per_page: 80,
+  //       page: pageNumber ? 1 : pageNumber,
+  //     });
+  //     const totalPages = Math.ceil(photos.total_results / photos.per_page);
+  //     setPageNumber(Math.floor(Math.random() * totalPages) + 1);
+  //     const photoArrlength = photos.photos.length;
+  //     const randomImageNumber = Math.floor(Math.random() * photoArrlength);
+  //     setAnimalURLImage(photos.photos[randomImageNumber].src.original);
+  //   }
 
+  async function fetchPhoto() {
+    try {
+      setIsLoadingImage(true);
+      const response = await fetch(
+        `https://api.pexels.com/v1/search?query=${query}&per_page=80&page=${
+          pageNumber ? 1 : pageNumber
+        }`,
+        {
+          headers: {
+            Authorization:
+              "FCHHpjKZb6AxS6BCzYSglJdx2ItT9KuYUtBPruiJkE9EypHJErJcbzPv",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const photos = await response.json();
+      console.log(photos);
+      const totalPages = Math.ceil(photos.total_results / photos.per_page);
+      setPageNumber(Math.floor(Math.random() * totalPages) + 1);
+      const photoArrlength = photos.photos.length;
+      const randomImageNumber = Math.floor(Math.random() * photoArrlength);
+      setAnimalURLImage(photos.photos[randomImageNumber].src.original);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+  console.log(pageNumber);
+  console.log(animalURLImage);
   function nextAnimalImage() {
     fetchPhoto();
   }
